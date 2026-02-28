5186ab7a61c6a7381743451947ba2cda14de2e6c
Ry Mam <73153805+creativemindsmusiccambo@users.noreply.github.com>
2026-03-01 05:32:20 +0700

    Update CORS middleware to allow all origins

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// CORS - Allow GitHub Pages
+// Allow all origins
 app.use(cors({
     origin: '*',
     credentials: true
