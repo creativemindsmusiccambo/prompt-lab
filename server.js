4764de912d293a5c2b62519eff58ff466af1c886
Ry Mam <73153805+creativemindsmusiccambo@users.noreply.github.com>
2026-03-03 20:00:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ Keywords: ${params.keywords}`;
     }
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
